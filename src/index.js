8ebee6d9d79b30ea508c0317b9f58703a2b4bbb6
randlos <mr.teddyfocus@gmail.com>
2019-01-04 18:46:18 +0100

    text-alignment and max-width 705px

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { timingSafeEqual } from "crypto";
+
 export let data = {};
 
 
@@ -8,7 +10,7 @@ export let state = {
 export function update() {
 
   // DATA for Testing
-  let data = [16, 23, 42];
+  let data = [110, 20, 12];
 
   data.sort(function(a, b){return a-b});
   //data.sort(d3.descending());
@@ -36,6 +38,7 @@ export function update() {
     //d3.scale.ordinal()
     //.domain(["6TH", "7TH", "5TH", "4TH"])
     //.range(colorbrewer.RdBu[12]);
+  
   let bar = chart.selectAll("g")
     .data(data)
     .enter().append("g");
@@ -62,14 +65,35 @@ export function update() {
 
   d3.selectAll("g")
     .append("text")
-    .attr("x", function(d) {
+    .attr("x", function(d,i) {
+
       let prev_perc = perc_so_far;
       let this_perc = 100*(d/total);
       perc_so_far = perc_so_far + this_perc;
+      
+      if (i == 1){
+        //let balkentext = Number.parseFloat(100*(d/total)).toFixed(1) + "%";
+        //let textcenter = (((prev_perc) * 705) / 100);
+        console.log(perc_so_far);
+        console.log(prev_perc);
+        console.log(this_perc);
+        console.log(perc_so_far - prev_perc);
+        console.log((perc_so_far - prev_perc)/2);
+        console.log(prev_perc + (perc_so_far - prev_perc)/2);
+        let centric = prev_perc + (perc_so_far - prev_perc)/2;
+        console.log(centric);
+        return centric + "%";
+      }
+
+      if (i == 2){
+        return "680px"; //(prev_perc + "%")
+      }
+
       //console.log("perc_so_far:" + perc_so_far + "; this_perc:" + this_perc + "; prev_perc:" + prev_perc + ";");
-      return prev_perc + "%";
+      return 22;
     })
-    
+
+
     //.attr("y", 11)
 
     // To middle/center in the bar-chart
@@ -79,7 +103,20 @@ export function update() {
     
     // .attr("fill", "#ffffff")
     // .attr("font-size", fontsize + "px")
-    .text(function(d) { return Number.parseFloat(100*(d/total)).toFixed(1) + "%"; });
+    .text(function(d) { 
+      let balkentext = Number.parseFloat(100*(d/total)).toFixed(1) + "%";
+      return balkentext; 
+  });
+
+  // d3.selectAll('g').selectAll("text").each(function(d,i){
+    
+  //   var firstChild = this.firstChild,
+  //       lastChild = this.lastChild;
+    
+  //   console.log(lastChild);
+
+  //   lastChild.attr("x", "705px");
+  // })
       
   // d3.select(window).on('resize', resize);
 
